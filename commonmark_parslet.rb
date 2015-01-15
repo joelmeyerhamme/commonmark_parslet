797e9b74351e3409f36892e3d318bd66adf1965c
Joel Meyer-Hamme <joelmeyerhamme@me.com>
2015-01-15 16:39:50 +0000

    repeat1

--- commonmark_parslet.rb
+++ commonmark_parslet.rb
@@ -1,5 +1,6 @@
 require 'parslet'
 require 'rspec'
+require 'parslet/rig/rspec'
 require './unicode_data'
 
 require 'byebug'
@@ -10,52 +11,19 @@ class CommonMark
       root(:line)
 
       rule(:line) { character.repeat }
-      rule(:character) { whitespace | punctuation | any }
+      rule(:character) { whitespace | any }
 
       rule(:whitespace_character) do
-        space | unicode_space | tab | carriage_return | newline
-      end
-
-      rule(:eol) do
-        carriage_return >> newline | newline | carriage_return
+        space | tab | newline
       end
 
       rule(:whitespace) do
-        whitespace_character.repeat >> whitespace_character.absent?
+        whitespace_character.repeat(1)
       end
 
       rule(:tab)             { str("\u0009") }
       rule(:space)           { str("\u0020") }
-      rule(:carriage_return) { str("\u000d") }
-      rule(:bom)             { str("\u0000") }
       rule(:newline)         { str("\u000a") }
-      rule(:blank_line)      { whitespace >> eol }
-
-      rule(:non_space) { space.absent? >> any }
-
-      rule(:ascii_punctuation) do
-        str('!')  | str('"') | str('#') | str('$') | str('%') | str('&')  |
-        str('\'') | str('(') | str(')') | str('*') | str('+') | str(',')  |
-        str('-')  | str('.') | str('/') | str(':') | str(';') | str('<')  |
-        str('=')  | str('>') | str('?') | str('@') | str('[') | str('\\') |
-        str(']')  | str('^') | str('_') | str('`') | str('{') | str('|')  |
-        str('}')  | str('|') | str('~')
-      end
-
-      rule(:unicode_punctuation) do
-        UnicodeData::CharClass["Pc", "Pd", "Pe", "Pf", "Pi", "Po", "Ps"].
-          map do |ch|
-            str(ch.to_i(16).chr('utf-8'))
-          end.reduce(:|)
-      end
-
-      rule(:unicode_space) do
-        UnicodeData::CharClass["Zs"].map do |ch|
-          str(ch.to_i(16).chr('utf-8'))
-        end.reduce(:|)
-      end
-
-      rule(:punctuation) { ascii_punctuation | unicode_punctuation }
     end
   end
 
@@ -72,53 +40,53 @@ describe CommonMark::Parser::Preliminaries do
   end
 
   it "should parse anything at all" do
-    expect(subject.parse(".")).to eq(".")
+    expect(subject).to parse(".")
   end
 
   it "should parse whitespace" # do
     # pending "freeze"
-    # expect(subject.parse("\u0009")).to eq("\u0009")
-    # expect(subject.parse("\u0020")).to eq("\u0020")
-    # expect(subject.parse("\u000d")).to eq("\u000d")
-    # expect(subject.parse("\u0000")).to eq("")
-    # expect(subject.parse("\u000a")).to eq("\u000a")
+    # expect(subject).to parse("\u0009")
+    # expect(subject).to parse("\u0020")
+    # expect(subject).to parse("\u000d")
+    # expect(subject).to parse("\u0000")
+    # expect(subject).to parse("\u000a")
   # end
 
   it "should parse ascii punctuation" # do
     # pending "freeze"
-    # expect(subject.parse('!')).to  eq('!')
-    # expect(subject.parse('"')).to  eq('"')
-    # expect(subject.parse('#')).to  eq('#')
-    # expect(subject.parse('$')).to  eq('$')
-    # expect(subject.parse('%')).to  eq('%')
-    # expect(subject.parse('&')).to  eq('&')
-    # expect(subject.parse('\'')).to eq('\'')
-    # expect(subject.parse('(')).to  eq('(')
-    # expect(subject.parse(')')).to  eq(')')
-    # expect(subject.parse('*')).to  eq('*')
-    # expect(subject.parse('+')).to  eq('+')
-    # expect(subject.parse(',')).to  eq(',')
-    # expect(subject.parse('-')).to  eq('-')
-    # expect(subject.parse('.')).to  eq('.')
-    # expect(subject.parse('/')).to  eq('/')
-    # expect(subject.parse(':')).to  eq(':')
-    # expect(subject.parse(';')).to  eq(';')
-    # expect(subject.parse('<')).to  eq('<')
-    # expect(subject.parse('=')).to  eq('=')
-    # expect(subject.parse('>')).to  eq('>')
-    # expect(subject.parse('?')).to  eq('?')
-    # expect(subject.parse('@')).to  eq('@')
-    # expect(subject.parse('[')).to  eq('[')
-    # expect(subject.parse('\\')).to eq('\\')
-    # expect(subject.parse(']')).to  eq(']')
-    # expect(subject.parse('^')).to  eq('^')
-    # expect(subject.parse('_')).to  eq('_')
-    # expect(subject.parse('`')).to  eq('`')
-    # expect(subject.parse('{')).to  eq('{')
-    # expect(subject.parse('|')).to  eq('|')
-    # expect(subject.parse('}')).to  eq('}')
-    # expect(subject.parse('|')).to  eq('|')
-    # expect(subject.parse('~')).to  eq('~')
+    # expect(subject).to parse('!')
+    # expect(subject).to parse('"')
+    # expect(subject).to parse('#')
+    # expect(subject).to parse('$')
+    # expect(subject).to parse('%')
+    # expect(subject).to parse('&')
+    # expect(subject).to parse('\'')
+    # expect(subject).to parse('(')
+    # expect(subject).to parse(')')
+    # expect(subject).to parse('*')
+    # expect(subject).to parse('+')
+    # expect(subject).to parse(',')
+    # expect(subject).to parse('-')
+    # expect(subject).to parse('.')
+    # expect(subject).to parse('/')
+    # expect(subject).to parse(':')
+    # expect(subject).to parse(';')
+    # expect(subject).to parse('<')
+    # expect(subject).to parse('=')
+    # expect(subject).to parse('>')
+    # expect(subject).to parse('?')
+    # expect(subject).to parse('@')
+    # expect(subject).to parse('[')
+    # expect(subject).to parse('\\')
+    # expect(subject).to parse(']')
+    # expect(subject).to parse('^')
+    # expect(subject).to parse('_')
+    # expect(subject).to parse('`')
+    # expect(subject).to parse('{')
+    # expect(subject).to parse('|')
+    # expect(subject).to parse('}')
+    # expect(subject).to parse('|')
+    # expect(subject).to parse('~')
   # end
 
   it "should parse unicode whitespace"
