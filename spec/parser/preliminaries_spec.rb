9d37475e6901f2dcce6426dbdd5d2e35e80814a4
Joël Meyer-Hamme <joel@meyer-hamme.com>
2015-01-17 16:30:06 +0000

    speedup

--- spec/parser/preliminaries_spec.rb
+++ spec/parser/preliminaries_spec.rb
@@ -1,29 +1,32 @@
 require './spec/spec_helper'
 
 describe CommonMark::Parser::Preliminaries do
-  it "should parse whitespace" do
-    is_expected.to parse("\t")
-    is_expected.to parse(" ")
-    is_expected.to parse("\r")
-    is_expected.to parse("\0")
-    is_expected.to parse("\n")
-  end
-
-  it "should parse ascii punctuation" do
-    CommonMark::Parser::Preliminaries.ascii_punctuation_chars.sample(3).each do |p|
-      is_expected.to parse(p)
+  describe "whitespace and punctuation" do
+    subject(:char) { described_class.new.character }
+    it "should parse whitespace" do
+      is_expected.to parse(" ")
+      is_expected.to parse("\t")
+      is_expected.to parse("\r")
+      is_expected.to parse("\0")
+      is_expected.to parse("\n")
     end
-  end
 
-  it "should parse unicode whitespace" do
-    CommonMark::Parser::Preliminaries.unicode_space_chars.sample(3).each do |ch|
-      is_expected.to parse(ch)
+    it "should parse ascii punctuation" do
+      CommonMark::Parser::Preliminaries.ascii_punctuation_chars.each do |p|
+        is_expected.to parse(p)
+      end
     end
-  end
 
-  it "should parse unicode punctuation" do
-    CommonMark::Parser::Preliminaries.unicode_punctuation_chars.sample(3).each do |ch|
-      is_expected.to parse(ch)
+    it "should parse unicode whitespace" do
+      CommonMark::Parser::Preliminaries.unicode_space_chars.each do |ch|
+        is_expected.to parse(ch)
+      end
+    end
+
+    it "should parse unicode punctuation" do
+      CommonMark::Parser::Preliminaries.unicode_punctuation_chars.each do |ch|
+        is_expected.to parse(ch)
+      end
     end
   end
 
