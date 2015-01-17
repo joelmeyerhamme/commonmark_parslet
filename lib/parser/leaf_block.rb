1305ef0c81a3c7549c1158d0a46620ea149c7e21
Joël Meyer-Hamme <joel@meyer-hamme.com>
2015-01-17 23:41:01 +0000

    setext headers

--- lib/parser/leaf_block.rb
+++ lib/parser/leaf_block.rb
@@ -1,9 +1,8 @@
 class CommonMark::Parser::LeafBlock < Parslet::Parser
-  root(:leaf_block)
+  root :leaf_block
 
-  def leaf_block_def
+  rule :leaf_block do
     CommonMark::Parser::LeafBlock::AtxHeader.new |
     CommonMark::Parser::LeafBlock::Hrule.new
   end
-  rule(:leaf_block) { leaf_block_def }
 end
--- lib/parser/leaf_block/atx_header.rb
+++ lib/parser/leaf_block/atx_header.rb
@@ -1,11 +1,10 @@
 class CommonMark::Parser::LeafBlock::AtxHeader < Parslet::Parser
-  root(:atx_header)
+  root :atx_header
 
-  def atx_header_def
+  rule :atx_header do
     pre.space_character.repeat(0,3) >> str("#").repeat(1,6) >> pre.whitespace_character.repeat(1,3) >> pre.character.repeat(1) >>
       (pre.whitespace.repeat(1) >> str('#').repeat(1)).maybe >> pre.eol >> pre.blank_line
   end
-  rule(:atx_header) { atx_header_def }
 
   def pre
     @@pre ||= CommonMark::Parser::Preliminaries.new
