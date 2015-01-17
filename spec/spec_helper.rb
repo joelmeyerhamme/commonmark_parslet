aeb8ca8bc42e22b380e0d2104d4ef1425f4c6605
Joël Meyer-Hamme <joel@meyer-hamme.com>
2015-01-17 15:10:01 +0000

    fixed strange coverage assertion on travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require 'bundler'
 Bundler.require(:default, :test)
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.minimum_coverage 100
 SimpleCov.start do
   add_group 'Libraries', 'lib'
   add_filter 'spec'
