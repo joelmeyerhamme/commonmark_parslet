f2565f87519307bd3e0336ad6c046a6f0133d8b2
Joël Meyer-Hamme <joel@meyer-hamme.com>
2015-01-17 16:03:54 +0000

    rules

--- lib/parser/leaf_block/hrule.rb
+++ lib/parser/leaf_block/hrule.rb
@@ -1,27 +1,27 @@
 class CommonMark::Parser::LeafBlock::Hrule < Parslet::Parser
   root(:hrule_line)
 
-  # rule(:hrule_line)
+  rule(:hrule_line)
   def hrule_line
     space_character.repeat(0,3) >> (hrule) >> whitespace.maybe
   end
 
-  # rule(:hrule)
+  rule(:hrule)
   def hrule
     hrule_char("*") | hrule_char("-") | hrule_char("_")
   end
 
-  # rule(:hrule_char)
+  rule(:hrule_char)
   def hrule_char(char)
     (str(char) >> whitespace.maybe).repeat(3)
   end
 
-  # rule(:whitespace)
+  rule(:whitespace)
   def whitespace
     CommonMark::Parser::Preliminaries.new.whitespace
   end
 
-  # rule(:space_character)
+  rule(:space_character)
   def space_character
     CommonMark::Parser::Preliminaries.new.space_character
   end
