8a6bacdecf3bb6f55d96e541da465d869640cb71
Joël Meyer-Hamme <joel@meyer-hamme.com>
2015-02-26 14:18:28 +0000

    indented_blocks

--- lib/parser/leaf_block/indented_block.rb
+++ lib/parser/leaf_block/indented_block.rb
@@ -2,15 +2,15 @@ class CommonMark::Parser::LeafBlock::IndentedBlock < Parslet::Parser
   root :indented_block
 
   rule :indented_block do
-    line >> (internal_blank_line | line).repeat(1)
+    chunk.repeat(1)
+  end
+
+  rule(:chunk) do
+    (pre.blank_line.absent? >> line).repeat(1) >> pre.blank_line
   end
 
   rule :line do
     pre.tab >> pre.line
-  end
-
-  rule :internal_blank_line do
-    (pre.tab.absent? >> pre.blank_line).repeat(1) >> pre.tab.present?
   end
 
   def pre
