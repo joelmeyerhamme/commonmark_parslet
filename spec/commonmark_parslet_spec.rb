0d0c98a03b3d1d5ad95bf2fbad6f4b28664d70f1
Joel Meyer-Hamme <joel@meyer-hamme.com>
2015-09-24 14:17:44 +0000

    folders

--- spec/commonmark_parslet_spec.rb
+++ spec/commonmark_parslet_spec.rb
@@ -1,13 +1,4 @@
-require './commonmark_parslet'
-Bundler.require(:test)
-require 'parslet/rig/rspec'
-require 'parslet/convenience'
-
-# SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-# SimpleCov.start do
-#   add_group 'Libraries', 'lib'
-#   add_filter 'spec'
-# end
+require './spec/spec_helper'
 
 describe CommonMark::Parser do
   describe 'hrule' do
