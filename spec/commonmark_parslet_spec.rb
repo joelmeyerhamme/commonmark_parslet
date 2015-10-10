faf75574991425627a585e18b24e745686f4d028
Joel Meyer-Hamme <joel@meyer-hamme.com>
2015-10-10 12:49:41 +0000

    lists depend upon newlines

--- spec/commonmark_parslet_spec.rb
+++ spec/commonmark_parslet_spec.rb
@@ -115,6 +115,7 @@ describe CommonMark::Parser do
   end
 
   it 'should parse ordered lists' do
+    pending 'implement newlines'
     expect(subject.parse("- hello\n- world")).to eq(
       {document: [
         {unordered_list: {inline: [{text: 'hello'}]}},
